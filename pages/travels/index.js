ab73af3b43d5cc0fd7e58ac5e2a7f70747d64a18
anujbarve <anujbarve27@gmali.com>
2021-08-02 23:15:03 +0530

    added details page and api

--- pages/travels/index.js
+++ pages/travels/index.js
@@ -1,9 +1,30 @@
-const Travels = () => {
+import styles from '../../styles/Travels.module.css';
+import Link from 'next/link';
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props:{
+            travels:data
+        }
+    }
+}
+
+const Travels = ({travels}) => {
     return ( 
         <div>
             <h1>
                 Travels
             </h1>
+            {travels.map(travel => (
+                <Link href={'/travels/' + travel.id} key={travel.id}>
+                    <a className={styles.single}>
+                        <h3>{travel.name}</h3>
+                    </a>
+                </Link>
+            ))}
         </div>
      );
 }
